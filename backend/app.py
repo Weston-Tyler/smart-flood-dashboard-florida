9b39550ce498ac5335b725d491bfccbfeefb20ae
Weston Tyler <wta1773@gmail.com>
2025-01-25 00:57:38 -0500

    Set up the backend service with minimal flask APP

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+# backend/app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Smart Flood Monitoring Backend is Running."
+
+@app.route('/api/test', methods=['GET'])
+def test_api():
+    return jsonify({"message": "API is working!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
